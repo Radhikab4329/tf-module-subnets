ef90a3d3a951242a17b58f3278241753b1ed2be0
Radhika <radhika.b4329@gmail.com>
2023-05-22 23:00:32 +0530

    update module subnet

--- vars.tf
+++ vars.tf
@@ -1,10 +1,3 @@
-variable "cidr_block" {}
-variable "availability_zone" {}
-variable "vpc_id" {}
-variable "env" {}
-variable "default_vpc_id" {}
-variable "name" {}
-variable "vpc_peering_connection_id" {}
 variable "internet_gw" {}
 variable "nat_gw" {}
 
